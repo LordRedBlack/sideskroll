1280bc5e78e095f20561b0370dcbd3e184d5275f
the16thpythonist <jonseb1998@gmail.com>
2021-10-09 18:17:13 +0200

    started hook

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    void Awake()
+    {
+        this.rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
